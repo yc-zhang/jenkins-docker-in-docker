b84cfb8243d55a46bcad0cfdcee3c1afc0982c04
yc-zhang <turalyon.zhangyc@gmail.com>
2018-08-25 21:27:36 +0800

    add a note.

--- run-jenkins.sh
+++ run-jenkins.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -eu
 
-docker rm -fv jenkins
+docker stop jenkins || true && docker rm -fv jenkins || true
 
 docker run -p 8080:8080 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   --name jenkins \
   --detach \
   jenkins/jenkins:lts
+
+echo "jenkins is started at port 8080 just now..."
