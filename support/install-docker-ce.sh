cfcb02e93ced619a5277765141c64b4fdd27dc6f
yc-zhang <turalyon.zhangyc@gmail.com>
2018-08-25 21:50:06 +0800

    Add install docker ce inner jenkins.

---
+++ support/install-docker-ce.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+# this script will be running inside of container from jenkins offical docker image
+# we will install docker-ce and use the daemon from the host machine
+# so, running `$ docker ps` will show you same result from host machine
+
+apt-get update && \
+apt-get -y install apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common && \
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey; apt-key add /tmp/dkey && \
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable" && \
+apt-get update && \
+apt-get -y install docker-ce
