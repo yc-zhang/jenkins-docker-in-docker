48780fa7ae016b32df530c1c5aadbfff037a2b48
yc-zhang <turalyon.zhangyc@gmail.com>
2018-08-29 23:04:22 +0800

    Fix the script to have volumn outside.

--- run-jenkins.sh
+++ run-jenkins.sh
@@ -3,7 +3,9 @@
 docker stop jenkins || true && docker rm -fv jenkins || true
 
 docker run -p 8080:8080 \
+  -u root \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/jenkins_home:/var/jenkins_home \
   --name jenkins \
   --detach \
   jenkins/jenkins:lts
